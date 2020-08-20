a3dadf5726096627d8a7ee369bad14de09aadcae
Xenofon Alexakos <xalexakos@gmail.com>
2020-08-21 02:31:44 +0300

    re-designed the pages' grid.

--- quiz/urls.py
+++ quiz/urls.py
@@ -4,5 +4,5 @@ from .views import home_page, quiz_page
 
 urlpatterns = [
     path('', home_page, name='home_page'),
-    path('quiz/', quiz_page, name='quiz'),
+    path('quiz/', quiz_page, name='quiz_page'),
 ]

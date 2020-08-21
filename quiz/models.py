8286dbdd6a430ec89d6ffcc04ddf6b1e285545e5
Xenofon Alexakos <xalexakos@gmail.com>
2020-08-21 14:45:44 +0300

    added str representation in all models + some tests

--- quiz/models.py
+++ quiz/models.py
@@ -8,12 +8,15 @@ class Quiz(models.Model):
 
     objects = QuizManager()
 
-    def __unicode__(self):
+    def __str__(self):
         return 'Quiz - %d' % self.id
 
 
 class Question(models.Model):
     text = models.TextField()
+
+    def __str__(self):
+        return self.text
 
 
 class Answer(models.Model):
@@ -21,8 +24,14 @@ class Answer(models.Model):
     text = models.TextField()
     is_correct = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.text
+
 
 class QuizQuestion(models.Model):
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     order = models.IntegerField(choices=[(i + 1, i + 1) for i in range(10)], default=0)
+
+    def __str__(self):
+        return '%s - %s' % (self.order, self.question)
--- tests/quiz/tests_models.py
+++ tests/quiz/tests_models.py
@@ -1,9 +1,30 @@
 from unittest import TestCase
 
-from quiz.models import Quiz
+from quiz.models import Quiz, Question, Answer, QuizQuestion
 
 
 class QuizModelTestCase(TestCase):
-    def test_unicode(self):
+    def test_str(self):
         quiz = Quiz.objects.create()
-        self.assertEqual(quiz.__unicode__(), 'Quiz - %d' % quiz.pk)
+        self.assertEqual(quiz.__str__(), 'Quiz - %d' % quiz.pk)
+
+
+class QuestionModelTestCase(TestCase):
+    def test_str(self):
+        question = Question.objects.create(text='What is the capital of the United States?')
+        self.assertEqual(question.__str__(), 'What is the capital of the United States?')
+
+
+class AnswerModelTestCase(TestCase):
+    def test_str(self):
+        question = Question.objects.create(text='What is the capital of the United States?')
+        answer = Answer.objects.create(question_id=question.id, text='Washington DC', is_correct=True)
+        self.assertEqual(answer.__str__(), 'Washington DC')
+
+
+class QuizQuestionModelTestCase(TestCase):
+    def test_str(self):
+        quiz = Quiz.objects.create()
+        question = Question.objects.create(text='What is the capital of the United States?')
+        quiz_question = QuizQuestion.objects.create(quiz_id=quiz.id, question_id=question.id, order=3)
+        self.assertEqual(quiz_question.__str__(), '3 - What is the capital of the United States?')
